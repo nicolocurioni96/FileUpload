5b35bc106af79fb219b78b84a98f918c4bdd940d
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-02-08 12:32:15 +0100

    Add the `DownloadTask` class

---
+++ FileUpload/Network/DownloadTask.swift
@@ -0,0 +1,21 @@
+//
+//  DownloadTask.swift
+//  FileUpload
+//
+//  Created by Nicolò Curioni on 08/02/23.
+//
+
+import Foundation
+
+class DownloadTask {
+    var file: File
+    var url: URL?
+    var inProgress = false
+    var resumeData: Data?
+    var task: URLSessionDataTask?
+    
+    init(file: File) {
+        self.file = file
+        self.url = URL(string: file.link)
+    }
+}
